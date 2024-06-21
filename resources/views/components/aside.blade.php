7a38bad1eee53f92c9232c60d7d4b4297d671a31
OmarGarDia <p3loporl@gmail.com>
2024-06-21 11:34:02 +0200

    Añadido para Lunes Actores/Actrices

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -46,5 +46,23 @@
 
             <a href="{{ route('directores') }}" class="hover:text-blue-500 w-full text-lg">Directores</a>
         </li>
+        <li
+            class="flex items-center border-t border-gray-200 dark:border-gray-700 border-b hover:bg-gray-200 dark:hover:bg-gray-700 p-4 w-full transition duration-300">
+            <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M17 10c2.761 0 5-2.239 5-5s-2.239-5-5-5-5 2.239-5 5 2.239 5 5 5zM17 12c-3.314 0-6 1.79-6 4v2h12v-2c0-2.21-2.686-4-6-4z">
+                </path>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M7 10c2.761 0 5-2.239 5-5S9.761 0 7 0 2 2.239 2 5s2.239 5 5 5zM7 12c-3.314 0-6 1.79-6 4v2h12v-2c0-2.21-2.686-4-6-4z">
+                </path>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M12 22c2.761 0 5-2.239 5-5s-2.239-5-5-5-5 2.239-5 5 2.239 5 5 5zM12 24c-3.314 0-6-1.79-6-4v-2h12v2c0 2.21-2.686 4-6 4z">
+                </path>
+            </svg>
+
+
+            <a href="" class="hover:text-blue-500 w-full text-lg">Actores/Actrices</a>
+        </li>
     </ul>
 </aside>
